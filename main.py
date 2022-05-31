489c7a92ed273d8824932cfefda15a005cc2452a
ben <80917990+ababen1@users.noreply.github.com>
2022-05-31 16:27:50 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import pywhatkit
+import json
+
+
+def send_posts(phone_num: str, db_path: str, max_jobs: int = 5):
+    # Opening JSON file
+    f = open(db_path, "r", encoding="utf8")
+    posts: dict = json.load(f)
+    msg_text = 'Jobs for you! \n'
+    for i in range(len(posts)):
+        if i >= max_jobs:
+            break
+        msg_text += list(posts.values())[i]
+    f.close()
+    pywhatkit.sendwhatmsg_instantly(phone_num, msg_text, wait_time=5, tab_close=True)
+
+
+send_posts("+972525563127", "postsDBProgrammers.json", 1)
