103467ee736d21d130e6815e295c5d8ac948d67e
ben <ben.egg.gov@gmail.com>
2022-06-07 13:17:10 +0300

    .

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import pywhatkit
 import json
-import selenium
 
 def send_posts(phone_num: str, db_path: str, max_jobs: int = 5):
     # Opening JSON file
     f = open(db_path, "r", encoding="utf8")
     posts: dict = json.load(f)
+    msg_text = ""
     for i in range(len(posts)):
         if i >= max_jobs:
             break
