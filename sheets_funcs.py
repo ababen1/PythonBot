2db62b7f76aa717857401f62cd6ad3bdbb541023
ben <ben.egg.gov@gmail.com>
2022-06-09 12:54:49 +0300

    added email sending

--- sheets_funcs.py
+++ sheets_funcs.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 from array import array
 from ast import keyword
 import enum
+import imp
 from multiprocessing.dummy import Array 
 
 import os.path
@@ -12,9 +13,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from pyparsing import Keyword
-
-# If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+from setup import creds
 
 SPREADSHEET_ID = '13zCT1ubfOCAejVlr5UwQNSp1XK3v3aiSopWkxjVLJp0'
 SAMPLE_RANGE_NAME = 'קבוצות מציאת עבודה - הייטק!A4:A'
@@ -44,27 +43,6 @@ KEYWORDS_DATA = {
     FIELD.ENTER_STUDIO: "מילות מפתח!A2:A"
     }
 
-
-def _search_for_credentials() -> dict:
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-    return creds
-
 def get_groups(field: FIELD) -> array:
     groups = []
     for group in get_spreadsheet_data(GROUPS_DATA[field]):
@@ -85,7 +63,6 @@ def get_keywords(field: FIELD) -> array:
     return keywords
 
 def get_spreadsheet_data(spreadsheet_range: str):
-    creds = _search_for_credentials()
     try:
         service = build('sheets', 'v4', credentials=creds)
 
--- whatsappbot.py
+++ whatsappbot.py
@@ -6,10 +6,11 @@ from traceback import print_list
 
 import pywhatkit
 from facebook_scraper import get_posts
+from send_email import gmail_send_message
 
 import sheets_funcs as sheets
 
-def send_posts(phone_num: str, db_path: str, max_jobs: int = 5):
+def send_posts_json(phone_num: str, db_path: str, max_jobs: int = 5):
     # Opening JSON file
     f = open(db_path, "r", encoding="utf8")
     posts: dict = json.load(f)
@@ -20,7 +21,17 @@ def send_posts(phone_num: str, db_path: str, max_jobs: int = 5):
         msg_text += list(posts.values())[i]
     f.close()
     pywhatkit.sendwhatmsg_instantly(phone_num, msg_text, wait_time=5)
-    
+
+def send_emails(posts: array, emails: array):
+    message_content = ""
+    message_subject = "{x} הצעות עבודה בשבילך".format(x = len(posts))
+    for post in posts:
+        message_content += "נכתב על ידי {user} ב {date}:".format(user = post["user_id"], date = post["time"]) + "\n"
+        message_content += post["text"] + "\n" + post["w3_fb_url"]
+        message_content += "\n\n"
+    gmail_send_message(emails, message_content, message_subject)
+        
+
 def get_posts_from_group(group_id, max_pages: int = 10):
     return get_posts(group=group_id, pages=max_pages)
 
@@ -50,6 +61,10 @@ def is_post_relevent(post: dict, keywords: array) -> bool:
             return True
     return False
 
-for field in sheets.FIELD:
-    relevent_posts = find_relevent_posts(field, 1)
-    print("found {x} relevent posts in {field}".format(x = len(relevent_posts), field = field.name))
+def main():
+    for field in sheets.FIELD:
+        relevent_posts = find_relevent_posts(field, 1)
+        print("found {x} relevent posts in {field}".format(x = len(relevent_posts), field = field.name))
+
+if __name__ == "__main__":
+    main()
