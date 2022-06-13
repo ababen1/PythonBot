7a7717a4c79ddd899b28861273d06d4df4570a39
ben <ben.egg.gov@gmail.com>
2022-06-13 17:31:13 +0300

    made the email look better

--- whatsappbot.py
+++ whatsappbot.py
@@ -4,6 +4,7 @@ from array import array
 from operator import truediv
 from tokenize import String, group
 from traceback import print_list
+from turtle import pos
 
 import pywhatkit
 from facebook_scraper import get_posts
@@ -11,28 +12,35 @@ from send_email import gmail_send_message
 
 import sheets_funcs as sheets
 
-def send_post_whatsapp(phone_num: str, post: dict):
-    msg_text = post_to_plain_text(post)
+def send_whatsapp_posts(phone_num: str, post: dict):
+    msg_text = posts_to_plain_text([post])
     pywhatkit.sendwhatmsg_instantly(phone_num, msg_text, wait_time=5)
 
-def send_emails(posts: array, emails: array):
-    message_content = ""
-    message_subject = "{x} הצעות עבודה בשבילך".format(x = len(posts))
-    for post in posts:
-        message_content += post_to_plain_text(post)
-        message_content += "\n-----------------------------------------------\n"
-    gmail_send_message(emails, message_content, message_subject, False)
+def send_email_posts(posts: array, emails: array, html_text: bool = True):
+    message_subject = "{x} הצעות עבודה חדשות".format(x = len(posts))
+    message_content = posts_to_html(posts) if html_text else posts_to_plain_text(posts)
+    with open("log.txt", "w", encoding='utf-8') as f:
+        f.write(message_content)
+    gmail_send_message(emails, message_content, message_subject, html_text)
 
-def post_to_plain_text(post: dict) -> str:
-    text = "נכתב על ידי {user} בתאריך {date}:".format(user = post["username"], date = post["time"]) + "\n"
-    text += post["text"] + "\n" + post["post_url"]
+def posts_to_plain_text(posts: array) -> str:
+    text = ""
+    for post in posts:
+        text = "נכתב על ידי {user} בתאריך {date}:".format(user = post["username"], date = post["time"]) + "\n"
+        text += post["text"] + "\n" + post["post_url"]
+        text += "\n--------------------------------------------------------------\n"
+    text += 'הודעה זו נשלחה אוטמטית ע"י מערכת מציאת עבודות של סטודיו אנטר' + '\n'
     return text
 
-def post_to_html(post: dict) -> str:
-    html_text = "<!DOCTYPE html><html dir=rtl><body>"
-    
+def posts_to_html(posts: array) -> str:
+    html_text = "<!DOCTYPE html><html lang='he' dir='rtl'><body>"
+    template = ""
+    with open("message_template.html",  "r", encoding='utf-8') as f:
+        template = f.read()
+    for post in posts:
+        html_text += template.format(user = post["username"], date = post["time"], content = post["text"], url = post["post_url"])
+    html_text += '<h5>הודעה זו נשלחה אוטמטית ע"י מערכת מציאת עבודות של סטודיו אנטר</h5>'
     return html_text + "</body></html>"
-
 
 def get_posts_from_group(group_id, max_pages: int = 10):
     posts = get_posts(group=group_id, pages=max_pages)
@@ -68,21 +76,33 @@ def is_post_relevent(post: dict, keywords: array) -> bool:
             return True
     return False
 
-def search_all_fields(max_posts: int = 10, pages_per_group: int = 10, save_to_file: bool = False) -> dict:
+def search_all_fields(
+    max_posts: int = 10, 
+    pages_per_group: int = 10, 
+    save_to_file: bool = False, 
+    send_emails: bool = True, 
+    send_whatsapps: bool = False
+) -> dict:
     results = {}
     for field in sheets.FIELD:
+        print("Searching for {f} jobs...".format(f = field.name))
         relevent_posts = find_relevent_posts(field, pages_per_group, max_posts)
         if save_to_file:
             with open("{f}_posts.json".format(f = field.name), 'w', encoding="utf-8") as file:
                 json.dump(relevent_posts, file, indent=4, ensure_ascii=False, default=str)
         print("found {x} relevent posts in {field}".format(x = len(relevent_posts), field = field.name))
+        if send_emails:
+            send_email_posts(relevent_posts, sheets.get_emails(field))
+            print("Emails sent!")
+        if send_whatsapps:
+            # TODO: Whatsapp handling
+            pass 
         results[field] = relevent_posts
     return results
 
 
 def main():
-    with open('TECH_posts.json', 'r', encoding="utf8") as f:
-        send_emails(json.load(f), ["ben.egg.gov@gmail.com"])
+    search_all_fields(save_to_file=True, send_emails=True)
 
 if __name__ == "__main__":
     main()
