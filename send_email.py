90ac326ac157556848e16e7f7a7b8fc241db291e
ben <ben.egg.gov@gmail.com>
2022-06-14 11:06:38 +0300

    no message

--- send_email.py
+++ send_email.py
@@ -10,14 +10,7 @@ from googleapiclient.errors import HttpError
 from setup import creds
 
 def gmail_send_message(recipents: array, content: str, subject: str, html: bool = True):
-    """Create and insert a draft email.
-       Print the returned draft's message and id.
-       Returns: Draft object, including draft id and message meta data.
-
-      Load pre-authorized user credentials from the environment.
-      TODO(developer) - See https://developers.google.com/identity
-      for guides on implementing OAuth2 for the application.
-    """
+    email_was_sent = True
     try:
         # create gmail api client
         service = build('gmail', 'v1', credentials=creds)
@@ -29,8 +22,6 @@ def gmail_send_message(recipents: array, content: str, subject: str, html: bool 
        
        # text
         mime_message.set_content(content, "html" if html else "plain")
-
-       
 
         # encoded message
         encoded_message = base64.urlsafe_b64encode(mime_message .as_bytes()).decode()
@@ -45,5 +36,5 @@ def gmail_send_message(recipents: array, content: str, subject: str, html: bool 
         print(F'Message Id: {send_message["id"]}')
     except HttpError as error:
         print(F'An error occurred: {error}')
-        send_message = None
-    return send_message
+        email_was_sent = False
+    return email_was_sent
--- whatsappbot.py
+++ whatsappbot.py
@@ -16,12 +16,11 @@ def send_whatsapp_posts(phone_num: str, post: dict):
     msg_text = posts_to_plain_text([post])
     pywhatkit.sendwhatmsg_instantly(phone_num, msg_text, wait_time=5)
 
-def send_email_posts(posts: array, emails: array, html_text: bool = True):
+def send_email_posts(posts: array, emails: array, html_text: bool = True) -> bool:
     message_subject = "{x} הצעות עבודה חדשות".format(x = len(posts))
     message_content = posts_to_html(posts) if html_text else posts_to_plain_text(posts)
-    with open("log.txt", "w", encoding='utf-8') as f:
-        f.write(message_content)
-    gmail_send_message(emails, message_content, message_subject, html_text)
+    was_sent = gmail_send_message(emails, message_content, message_subject, html_text)
+    print("Emails sent!" if was_sent else "Error while sending emails")
 
 def posts_to_plain_text(posts: array) -> str:
     text = ""
@@ -29,7 +28,6 @@ def posts_to_plain_text(posts: array) -> str:
         text = "נכתב על ידי {user} בתאריך {date}:".format(user = post["username"], date = post["time"]) + "\n"
         text += post["text"] + "\n" + post["post_url"]
         text += "\n--------------------------------------------------------------\n"
-    text += 'הודעה זו נשלחה אוטמטית ע"י מערכת מציאת עבודות של סטודיו אנטר' + '\n'
     return text
 
 def posts_to_html(posts: array) -> str:
@@ -61,7 +59,7 @@ def find_relevent_posts(field: sheets.FIELD, pages_per_group: int = 10, max_post
         curent_group_posts = get_posts_from_group(current_group, pages_per_group)
         for post in curent_group_posts:
             # save the post if it's relevent
-            if is_post_relevent(post, keywords):
+            if is_post_relevent(post, keywords, field):
                 relevent_posts.append(post)
                 # check if we reached max_posts
                 if max_posts > 0 and len(relevent_posts) >= max_posts:
@@ -70,16 +68,31 @@ def find_relevent_posts(field: sheets.FIELD, pages_per_group: int = 10, max_post
     return relevent_posts   
 
 # determines if a post is relevent or not, according to keywords
-def is_post_relevent(post: dict, keywords: array) -> bool:
+def is_post_relevent(post: dict, keywords: array, field: sheets.FIELD) -> bool:
+    # Check if the post has been sent before. if it was, it's not relevent
+    for sent_post in get_sent_posts(field):
+        if sent_post["post_id"] == post["post_id"]:
+            return False
+
+    # Check if one of the keywords is in the post
     for keyword in keywords:
         if keyword in post["post_text"]:
             return True
     return False
 
+def get_sent_posts(field: sheets.FIELD) -> array:
+    with open("{f}_posts.json".format(f = field.name), 'r', encoding="utf-8") as file:
+        return json.load(file)
+
+def add_to_sent_posts(new_posts: array, field: sheets.FIELD):
+    sent_posts = get_sent_posts(field)
+    sent_posts.append(new_posts)
+    with open("{f}_posts.json".format(f = field.name), 'w', encoding="utf-8") as file:
+        json.dump(sent_posts, file, indent=4, ensure_ascii=False, default=str)
+
 def search_all_fields(
     max_posts: int = 10, 
     pages_per_group: int = 10, 
-    save_to_file: bool = False, 
     send_emails: bool = True, 
     send_whatsapps: bool = False
 ) -> dict:
@@ -87,22 +100,22 @@ def search_all_fields(
     for field in sheets.FIELD:
         print("Searching for {f} jobs...".format(f = field.name))
         relevent_posts = find_relevent_posts(field, pages_per_group, max_posts)
-        if save_to_file:
-            with open("{f}_posts.json".format(f = field.name), 'w', encoding="utf-8") as file:
-                json.dump(relevent_posts, file, indent=4, ensure_ascii=False, default=str)
         print("found {x} relevent posts in {field}".format(x = len(relevent_posts), field = field.name))
+        was_sent = False
         if send_emails:
-            send_email_posts(relevent_posts, sheets.get_emails(field))
-            print("Emails sent!")
+            was_sent =  True if send_email_posts(relevent_posts, sheets.get_emails(field)) else was_sent
         if send_whatsapps:
             # TODO: Whatsapp handling
             pass 
+        if was_sent:
+            add_to_sent_posts(relevent_posts, field)
         results[field] = relevent_posts
     return results
 
 
 def main():
-    search_all_fields(save_to_file=True, send_emails=True)
+    search_all_fields(send_emails=True, max_posts=-1, pages_per_group=5)
+    print("done!")
 
 if __name__ == "__main__":
     main()
